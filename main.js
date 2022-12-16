6a69d441a0cb6e41abf8134bb63fbdb98a475a38
peeler <t.delourme11@gmail.com>
2022-12-16 21:44:50 +0100

    error post again

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
 import "./style.css";
 
+import axios from "axios";
+
+export {};
+
+const form = document.getElementById("form");
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(form);
+  const formEntries = formData.entries();
+  const data = Object.fromEntries(formData.entries());
+
+  const user = await axios.post("http://0.0.0.0:3000/users", data);
+
+  console.log(user.data);
+
+  alert(`Successfully registered! Your user id is: ${user.data.id}`);
+});
